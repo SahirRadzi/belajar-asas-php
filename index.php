3b48899a50cd5cc5bed608810f9e2c0759aebdc3
SahirRadzi <sahir.radzi@gmail.com>
2025-07-17 00:00:08 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP - latihan 1 variables</title>
+</head>
+<body>
+
+    <h1 style="text-align:center">Belajar Asas PHP</h1>
+
+    <?php
+
+        $Nama = "Ali";
+        $nama = "Abu";
+
+
+        echo 'Nama saya '. $Nama .' dan nama kawan saya '. $nama; 
+
+    ?>
+    
+    
+</body>
+</html>
