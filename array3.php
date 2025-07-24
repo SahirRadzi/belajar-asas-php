887634b2aaaee1077365ae5a9e0bee594e8ef3d5
SahirRadzi <sahir.radzi@gmail.com>
2025-07-24 22:09:52 +0800

    add comment

--- array3.php
+++ array3.php
@@ -1,15 +1,15 @@
 <?php 
 
-$murid = [];
+$murid = []; // array null
 
-$murid[] = 'Ali';
-$murid[] = 'Abu';
-$murid[] = 'Aliyah';
-$murid[] = 'Amira';
+$murid[] = 'Ali'; // array 0
+$murid[] = 'Abu'; // array 1
+$murid[] = 'Aliyah'; // array 2
+$murid[] = 'Amira'; // array 3
 
 
 echo $murid[0] . ' + ' . $murid[1] . ' (' . $murid[2] . ' + ' . $murid[3] . ') ';
-echo "<br> ------------ </br>";
+echo "<br> ------------ </br>"; // breakdown
 
 
 echo $murid[0] .' '."dan".' '. $murid[1] .' '."adalah kawan baik. Mereka juga mempunyai kawan perempuan bernama" .' '.$murid[2] . " dan " . $murid[3];
