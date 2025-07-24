cf233e4bf827d3e5d92cb9283cdc90a1cd1010bd
SahirRadzi <sahir.radzi@gmail.com>
2025-07-24 22:06:51 +0800

    tambah lagi array[2][3]

--- array3.php
+++ array3.php
@@ -4,13 +4,15 @@ $murid = [];
 
 $murid[] = 'Ali';
 $murid[] = 'Abu';
+$murid[] = 'Aliyah';
+$murid[] = 'Amira';
 
 
-echo $murid[0] . ' + ' . $murid[1];
+echo $murid[0] . ' + ' . $murid[1] . ' (' . $murid[2] . ' + ' . $murid[3] . ') ';
 echo "<br> ------------ </br>";
 
 
-echo $murid[0] .' '."dan".' '. $murid[1] .' '."adalah kawan baik.";
+echo $murid[0] .' '."dan".' '. $murid[1] .' '."adalah kawan baik. Mereka juga mempunyai kawan perempuan bernama" .' '.$murid[2] . " dan " . $murid[3];
 
 
 echo "<pre>";
