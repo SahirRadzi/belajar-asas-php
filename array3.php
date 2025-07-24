e3d80c195e89b0d10bfeb053babb44a1b4f2943b
SahirRadzi <sahir.radzi@gmail.com>
2025-07-24 20:09:19 +0800

    cuba membuat 1 ayat guna array PHP

--- array3.php
+++ array3.php
@@ -2,6 +2,17 @@
 
 $murid = [];
 
+$murid[] = 'Ali';
+$murid[] = 'Abu';
+
+
+echo $murid[0] . ' + ' . $murid[1];
+echo "<br> ------------ </br>";
+
+
+echo $murid[0] .' '."dan".' '. $murid[1] .' '."adalah kawan baik.";
+
+
 echo "<pre>";
 
     print_r($murid);
